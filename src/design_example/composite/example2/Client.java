f88d84964364cbe35bbe69f71b664ecb58303ad2
hzqiuxm <hzqiuxm@163.com>
2015-12-05 10:36:35 +0800

    add Visitor

---
+++ src/design_example/composite/example2/Client.java
@@ -0,0 +1,7 @@
+package design_example.composite.example2;
+
+/**
+ * Created by hzqiuxm on 2015/12/5 0005.
+ */
+public class Client {
+}
---
+++ src/design_example/visitor/example2/Client.java
@@ -0,0 +1,7 @@
+package design_example.visitor.example2;
+
+/**
+ * Created by hzqiuxm on 2015/12/5 0005.
+ */
+public class Client {
+}
