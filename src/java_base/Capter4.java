0e5efc56c24d8b6af8877b321fa05a3ee02d3062
hzqiuxm <qiuxiaomin@yunhetong.net>
2016-02-18 17:41:34 +0800

    update java_base capter4

--- src/java_base/Capter4.java
+++ src/java_base/Capter4.java
@@ -4,7 +4,11 @@ package java_base;
  * Created by hzqiuxm on 2016/2/14 0014.
  */
 public class Capter4 {
+    public static void m(){
+        System.out.println("m()");
+    }
     public static void main(String ...  args) {
-
+        Capter4 c4 = null;
+        c4.m();
     }
 }
