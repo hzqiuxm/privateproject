94a08850736041bc2515f2e817e4fab314c2f9c5
qiuding <880807>
2016-02-19 22:22:41 +0800

    update capter5

--- src/java_base/Capter5.java
+++ src/java_base/Capter5.java
@@ -2,11 +2,27 @@ package java_base;
 
 /**
  * Created by hzqiuxm on 2016/2/14 0014.
+ * 枚举类
  */
 public class Capter5 {
 
     public static void main(String[] args) {
-
-
+        Capter5 c5 = new Capter5();
+        c5.go(Direction.EAST);
+//        c5.go(5);
+//        c5.go(Color.BLUE);
     }
+
+    public void go(Direction d){
+        System.out.println("go to :" + d);
+    }
+
+    enum Direction{
+        NORTH, SOURTH, EAST, WEST
+    }
+
+    enum Color{
+        GREEN, RED, YELLOW, BLUE
+    }
+
 }
