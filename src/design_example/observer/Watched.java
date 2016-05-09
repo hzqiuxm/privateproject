160ad59b8f4f48305225ddf3a566ea05f9632dab
hzqiuxm <qiuxiaomin@yunhetong.net>
2016-05-09 20:03:28 +0800

    update boserver

--- src/design_example/observer/Watched.java
+++ src/design_example/observer/Watched.java
@@ -14,8 +14,14 @@ public class Watched extends Observable {
     public void changeData(String data){
         if(!this.data.equals(data)){
             this.data = data;
+            //注意在使用java的Observer模式的时候，这句话不可少
             setChanged();
         }
+
+        //使用了拉的方式,默认将目标对象传给观察者对象
         notifyObservers();
+
+        //也可以使用推方式，把需要的内容传给观察者
+        //notifyObservers(Object org);
     }
 }
